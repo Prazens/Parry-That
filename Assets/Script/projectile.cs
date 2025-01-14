2b15b5a1fe7b1b548d501d8cb2eaa67c4ea007a6
dongin2322 <dongin2322@korea.ac.kr>
2025-01-14 23:26:47 +0900

    Revert "Merge pull request #3 from Prazens/feature/judge-and-score"
    This reverts commit d3917ff9b9adbfb17a40ae74bfb33f5b27db7bf7, reversing
    changes made to eb7e7f22ebf51094d332a70051c171783e93d17a.

--- Assets/Script/projectile.cs
+++ Assets/Script/projectile.cs
@@ -8,29 +8,19 @@ public class projectile : MonoBehaviour
     public Transform target; // 플레이어 위치 (중앙)
     public float speed = 5.0f; // 노트 이동 속도
     public StrikerController owner; // 상위 striker
-    public NoteData noteData; // 노트의 정보
-    public ScoreManager scoreManager;
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime); // player를 향해서 이동
-
-        if (StageManager.Instance.currentTime - noteData.arriveTime > 0.12d)
+        if (Vector3.Distance(transform.position, target.position) < 0.1d) // 일정거리 가까워지면 destroy로 임시 구현
         {
-            scoreManager.JudgeManage(owner.location, 0);
-            Destroy(owner.projectileQueue.Dequeue());
+            Debug.Log("Missed Note!");
+
+            // 저장 해제
+            owner.projectileQueue.Dequeue();
+
+            Destroy(gameObject);
         }
-
-        // 일정거리 가까워지면 destroy로 임시 구현
-        // if (Vector3.Distance(transform.position, target.position) < 0.1d)
-        // {
-        //     Debug.Log("Missed Note!");
-
-        //     // 저장 해제
-        //     owner.projectileQueue.Dequeue();
-
-        //     Destroy(gameObject);
-        // }
     }
 }
