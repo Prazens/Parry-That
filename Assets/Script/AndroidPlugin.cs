5c1c7c75a8059c1ac7fc7273a7d0bf785fbc1917
홍성범 <sungbum132@naver.com>
2025-01-24 01:46:58 +0900

    안드로이드 커스텀 진동 기능 추가

---
+++ Assets/Script/AndroidPlugin.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidPlugin : MonoBehaviour
+{
+    private AndroidJavaObject ajo;
+
+    private void Awake() =>
+        ajo = new AndroidJavaObject(className: "com.example.unityvibration.UnityVibration");
+
+    public void Vibrate(long duration, int amplitude)
+    {
+        ajo.Call(methodName: "vibration", duration, amplitude);
+    }
+
+    // Vibrate(duration: x, amplitude: y);
+    // -> x ms 길이, 세기 y (0~255)의 진동 생성함
+
+    // 진동 적용할 부분 작성
+    // 아래 ex: 터치할 때마다 진동 생성
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vibrate(duration: 100, amplitude: 128);
+        }
+    }
+}
