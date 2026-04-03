025a367d943ab503679819b2099c99f4d1ab70aa
tonyxhsl <kimtaehyun258@gmail.com>
2026-04-03 21:06:05 +0900

    게임 중 점수가 제대로 나오지 않는 문제 수정

--- Assets/2.Scripts/Gameplay/TouchAndJudge/JudgeSystem.cs
+++ Assets/2.Scripts/Gameplay/TouchAndJudge/JudgeSystem.cs
@@ -467,7 +467,6 @@ public class JudgeSystem : MonoBehaviour
         // 가드 시 처리
         else if (judgeType == JudgeType.LateBlocked || judgeType == JudgeType.EarlyBlocked)
         {
-            dynamicUIManager?.DisplayScore(score);
             playerManager.PlayerBlockedSound();
         }
         // 패링 성공 시 처리
@@ -497,6 +496,7 @@ public class JudgeSystem : MonoBehaviour
                     parriedProjectileManager.ParryTusache(judgeable.noteDirection, (int)judgeable.attackType);
                 }
             }
+            dynamicUIManager?.DisplayScore(score);
         }
 
         // 대상 노트 제거
